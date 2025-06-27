8310a8fd707ceac23dc72c09063cbffafd8e8bba
MathPerson12321 <mathguy12321@gmail.com>
2025-06-26 20:35:50 -0600

    pls

--- public/display.js
+++ public/display.js
@@ -166,7 +166,7 @@ function calcTime(){
 }
 
 async function updateAll(){
-  data = await fetchJSON("/gamedata",gamenum);
+  data = await fetchJSON("/gamedata",gamenum).data;
   reaps = await fetchJSON("/reaps",gamenum);
   reaps = Object.entries(reaps).filter(([_, val]) => val !== null);
   userlastreaps = await fetchJSON("/lastuserreap",gamenum);
