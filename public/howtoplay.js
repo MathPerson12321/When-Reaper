d5e0ec053174cfa7644da03d1278e53f9994e632
MathPerson12321 <mathguy12321@gmail.com>
2025-08-04 18:19:02 -0600

    header to intro

--- public/howtoplay.js
+++ public/howtoplay.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
     let info = `
     <div id="information">
+        <h2>How to Play</h2>
         <p>Welcome to When Reaper, a universe all about time! Here, you must steal time from the mainframe by "reaping" and add it to your collection, however, you must recharge (which takes some amount of time) before being able to take time again.</p>
         <p>However, there are extra natural events this universe stores in its space, including ones that make your time more valuable or less valuable.</p>
         <p>It will also randomly generate a bomb using the leftover toxins from other people taking time. This allows you to dazzle the other person and take the time for yourself (that they were originally going to take).</p>
@@ -11,7 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
     info = `
     <div id="announcements">
         <h2>Announcements</h2>
-        <p><b>The Game 3 page is currently unavailable due to an issue, the game will be delayed.</b></p>
+        <p><b>Game 3 has been delayed, and the win time and cooldown will not be revealed.</b></p>
     </div>
     `;
     document.getElementById("information").insertAdjacentHTML("afterend", info);
