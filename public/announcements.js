14454a1f487120662399e0aaae521f71669b05be
MathPerson12321 <mathguy12321@gmail.com>
2025-08-05 17:26:04 -0600

    announcement

--- public/announcements.js
+++ public/announcements.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let info = `
     <div id="announcements">
         <h2>Announcements</h2>
-        <p><b>Game 3 has been delayed, and the win time and cooldown will not be revealed.</b></p>
+        <p><b>Maintenence will happen at 8 PM ET and will end at 9 PM ET (could be earlier).</b></p>
     </div>
     `;
     document.getElementById("information").insertAdjacentHTML("afterend", info);
