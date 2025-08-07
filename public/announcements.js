16c186bbc41c4ba870d0b32605be00b5487b531f
MathPerson12321 <mathguy12321@gmail.com>
2025-08-07 00:31:34 -0600

    new announce

--- public/announcements.js
+++ public/announcements.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", async() => {
     <div id="announcements">
         <h2>Announcements</h2>
         <p><b>You can now sign-up with a username, email, and password instead of google! Logging out + alt detection is the next update.</p>
-        <p>Good luck in Game 3!</p></b>
+        <p>Good luck in Game 3! Free reaps are now a thing.</p></b>
     </div>
      `
     document.getElementById("information").insertAdjacentHTML("afterend", res);
