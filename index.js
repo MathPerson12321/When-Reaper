4a66432f30558cec534d9e601f44cfb262321b55
MathPerson12321 <mathguy12321@gmail.com>
2025-08-09 13:10:43 -0600

    sign in with google work

--- index.js
+++ index.js
@@ -157,7 +157,6 @@ async function addUser(user, id) {
 }
 
 async function addUserWithPass(user,password,id) {
-  console.log({user,password,id})
   const existing = await firestore
     .collection("users")
     .where("username","==", user)
