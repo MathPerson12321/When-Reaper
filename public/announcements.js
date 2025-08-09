9393a7ee8a33d2225ee414aa7a3c69f374031bb3
MathPerson12321 <mathguy12321@gmail.com>
2025-08-08 21:59:43 -0600

    new announcement

--- public/announcements.js
+++ public/announcements.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", async() => {
     let res = `
     <div id="announcements">
         <h2>Announcements</h2>
-        <p><b>Major bug in logging in and creating accounts, Game 3 has been DELAYED to TBD.</p>
+        <p><b>The login bug and creating account with a password is now fixed, Game 3 will be pushed back a bit.</b></p>
         <p>Good luck in Game 3! Free reaps are now a thing.</p></b>
     </div>
      `
