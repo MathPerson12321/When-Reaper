509b78522a00fc6e353b1303ec07d6ce08cc9fe9
MathPerson12321 <mathguy12321@gmail.com>
2025-08-11 22:01:06 -0600

    misspelling

--- index.js
+++ index.js
@@ -686,8 +686,8 @@ wss.on("connection", (ws) => {
 async function getMaintenanceData(){
   const doc = await firestore.collection("settings").doc("maintenence").get();
   const data = doc.data()
-  const startTimestamp = data.maintenancestart.seconds*1000;
-  const endTimestamp = data.maintenanceend.seconds*1000;
+  const startTimestamp = data.maintenencestart.seconds*1000;
+  const endTimestamp = data.maintenenceend.seconds*1000;
   return {start:startTimestamp,end:endTimestamp};
 }
 app.get("/maintenancedata", async (req, res) => {
