737b651c52e37e86f9fac2ea3a74c92b6a071af8
MathPerson12321 <mathguy12321@gmail.com>
2025-08-11 22:23:56 -0600

    random check

--- public/maintenence.js
+++ public/maintenence.js
@@ -3,15 +3,13 @@ document.addEventListener("DOMContentLoaded", async() => {
     const response = await fetch(link+"maintenancedata");
     if(response.ok){
         const data = await response.json();
-        if (data.maintenance) {
-            const start = new Date(data.start).toLocaleString();
-            const end = new Date(data.end).toLocaleString();
-            
-            // Display the maintenance details on the page
-            document.getElementById("maintenance-status").innerHTML = `
-            Maintenance started at ${start} and ends at ${end}.
-            `;
-        }
+        const start = new Date(data.start).toLocaleString();
+        const end = new Date(data.end).toLocaleString();
+        
+        // Display the maintenance details on the page
+        document.getElementById("maintenance-status").innerHTML = `
+        Maintenance started at ${start} and ends at ${end}.
+        `;
     }
 });
   
